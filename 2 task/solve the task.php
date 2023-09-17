6185ed7a423b7ecb140cf255cb37d15d52b0cffa
bodegg <bodemansour8@gmail.com>
2023-09-17 17:44:54 +0300

    file task 2

---
+++ 2 task/solve the task.php
@@ -0,0 +1,40 @@
+<?php
+
+# string data type
+$str = 'my name is abdalla ';
+
+# string data type in constant
+define('AGE', 'and my age is ');
+
+# integer data type
+$int = 22;
+
+# float | double data type
+$float = .6;
+
+# boolean data type
+$active = true;
+
+/**
+ * this function say hello 'username'
+ * 
+ * 1 - use all variable 
+ * 2 - use all constant
+ * 3 - use if condition
+ * 4 - return string
+ */
+
+function test_func(): string
+{
+  global $str, $int, $float, $active;
+  if ($active) {
+    $string = $str . AGE . $int + $float;
+    return $string;
+  }
+}
+
+echo test_func();
+echo "<br>################<br>";
+echo gettype(test_func());
+echo "<br>################<br>";
+var_dump(test_func());
