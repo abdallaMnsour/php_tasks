335082fab7854c2363a0d6137c938f590f49b445
bodegg <bodemansour8@gmail.com>
2023-09-30 20:18:14 +0300

    add new file about us

---
+++ 4 task/aboutus.php
@@ -0,0 +1,8 @@
+<?php include 'layout/header.php' ?>
+<h1 class="mt-4 mb-5 text-center text-primary">hello user</h1>
+<p class="w-50 text-center m-auto">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae recusandae explicabo sint deserunt, aspernatur ipsa nisi eligendi impedit temporibus, quas iste tenetur nam non. Itaque molestias adipisci minus vel libero?
+  <a href="index.php">home</a>
+</p>
+
+<?php include 'layout/footer.php' ?>
--- 4 task/index.php
+++ 4 task/index.php
@@ -2,7 +2,10 @@
 include 'layout/header.php';
 require_once 'userData/data.php';
 ?>
-<h1 class="text-secondary m-3">Canada states...</h1>
+<div class="d-flex justify-content-between align-items-center m-5">
+  <h1 class="text-secondary m-3">Canada states...</h1>
+  <a href="aboutus.php" class="btn btn-primary">aboutUs</a>
+</div>
 <?php foreach ($stateList['CA'] as $key => $value) : ?>
 
   <p class="alert alert-primary w-50 m-3 mb-2"><?= '[ ' . $key . ' ] -> ' . $value ?></p>
