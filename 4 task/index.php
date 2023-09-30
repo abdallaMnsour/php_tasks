3a421f8cf8e93e1ac9243d241577eb948ba9c3c6
Abdalla Mansour <108527893+bode-mansour@users.noreply.github.com>
2023-09-30 22:14:05 +0300

    Update index.php

--- 4 task/index.php
+++ 4 task/index.php
@@ -2,15 +2,14 @@
 include 'layout/header.php';
 require_once 'userData/data.php';
 ?>
+
 <div class="d-flex justify-content-between align-items-center m-5">
   <h1 class="text-secondary m-3">Canada states...</h1>
   <a href="aboutus.php" class="btn btn-primary">aboutUs</a>
 </div>
+
 <?php foreach ($stateList['CA'] as $key => $value) : ?>
-
-  <p class="alert alert-primary w-50 m-3 mb-2"><?= '[ ' . $key . ' ] -> ' . $value ?></p>
-
-<?php
-endforeach;
+  <p class="alert alert-primary w-50 m-3 mb-2"><?= "[ $key ] -> " . $value ?></p>
+<?php endforeach;
 include 'layout/footer.php';
 ?>
