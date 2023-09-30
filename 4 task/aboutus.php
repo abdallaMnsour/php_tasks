841b280eecaff1999e07c6edbdd3c9cc4225f5c5
Abdalla Mansour <108527893+bode-mansour@users.noreply.github.com>
2023-09-30 22:12:36 +0300

    Update aboutus.php

--- 4 task/aboutus.php
+++ 4 task/aboutus.php
@@ -1,4 +1,5 @@
 <?php include 'layout/header.php' ?>
+
 <h1 class="mt-4 mb-5 text-center text-primary">hello user</h1>
 <p class="w-50 text-center m-auto">
   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae recusandae explicabo sint deserunt, aspernatur ipsa nisi eligendi impedit temporibus, quas iste tenetur nam non. Itaque molestias adipisci minus vel libero?
